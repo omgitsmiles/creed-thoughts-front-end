41ef422820f6e6cc67182f3c028e6cb77f86706d
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-23 22:07:47 -0400

    css
    material ui icons

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import ClearIcon from '@mui/icons-material/Clear';
+import { Clear } from '@mui/icons-material';
+import EditIcon from '@mui/icons-material/Edit';
 
 const Message = ({ post }) => {
 
   return (
     <div>
-    <main className="postBorder">{post.message}</main>
+    <main className="postBorder">{post.message} <ClearIcon fontSize='small' onClick={() => console.log("Hi")}/> <EditIcon fontSize='small'/></main>
+    <div></div>
     </div>
   )
 }
