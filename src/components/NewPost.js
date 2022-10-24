37657cb58c3547ec80139e6b8542b1a306fb17cb
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-23 23:51:05 -0400

    added form
    for blog post

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import TextField from '@mui/material/TextField';
 
 const NewPost = () => {
+  const [blogPost, setBlogPost] = useState("")
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log(blogPost)
+  }
+
   return (
-    <div>NewPost</div>
+    <>
+    <form onSubmit={handleSubmit}>
+    <Box
+    sx={{
+      '& .MuiTextField-root': { m: 1, width: '25ch' },
+    }}
+    noValidate
+    autoComplete="off"
+    >
+      <br></br>
+      <br></br>
+    <TextField 
+          className="blogPost"
+          id="outlined-multiline-static"
+          label="Post"
+          multiline
+          rows={10}
+          defaultValue={blogPost}
+          onChange={(e) => setBlogPost(e.target.value)}
+        />
+    </Box>
+    <Fab variant="extended" color="primary" aria-label="add" className="postBtn">
+        Post
+    </Fab>
+    </form>
+    </>
   )
 }
 
