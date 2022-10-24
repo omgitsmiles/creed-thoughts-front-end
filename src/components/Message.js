0556e25a26ccc8ae9af266f8337945af4887ba92
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-24 10:00:35 -0400

    opened an edit form
    created an edit form

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ClearIcon from '@mui/icons-material/Clear';
-
 import EditIcon from '@mui/icons-material/Edit';
 
 const Message = ({ post:{message, user, id}, handleDelete }) => {
+    const [onEdit, isOnEdit] = useState(false)
+
+
+    const editForm = (
+        <form>
+          <input
+            name="content"
+            type="text"
+            // value={editInputValue.content}
+            // onChange={handleEditChange}
+          />
+          <input type="submit" />
+        </form>
+      );
 
   return (
     <div>
     <main className="postBorder">{message} -{user.username}
     <ClearIcon fontSize='small' className="editPosts" onClick={() => handleDelete(id)}/> 
-    <EditIcon fontSize='small' className="editPosts"/></main>
+    <EditIcon fontSize='small' className="editPosts" onClick={() => isOnEdit(onEdit => !onEdit)}/>
+    {!onEdit ? (
+          null
+        ) : (
+          editForm
+        )}
+    </main>
     <div></div>
     </div>
   )
