8d8bddd0be9325e00b211185add4560e23eb6a2d
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-23 20:50:41 -0400

    styling
    styled nav and body

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className="navHeader">
+        www.creedthoughts.gov.www\creedthoughts
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
